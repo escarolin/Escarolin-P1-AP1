7c201f0642587728587bb7362cfa0044aae5935b
escarolin <escarolin09@hotmail.com>
2020-10-05 19:32:49 -0400

    creando la BLL

--- BLL/CuidadesBLL.cs
+++ BLL/CuidadesBLL.cs
@@ -11,19 +11,144 @@ using Microsoft.EntityFrameworkCore;
 using Escarolin_P1_AP1.Entidades;
 using Escarolin_P1_AP1.DAL;
 
+
 namespace Escarolin_P1_AP1.BLL{
     public class CuidadesBLL{
 
+       //EXISTE
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Cuidades.Any(d => d.CuidadId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
 
-         public static bool Guardar(Cuidades cuidades){
-             if(!Existe(cuidades.CuidadesId))
-             return Insertar(cuidades);
-             else{
-                 return Modificar (cuidades);
-             }
-         }
+        //INSERTAR
+        private static bool Insertar(Cuidades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Cuidades.Add(ciudades);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
 
-        private static bool Insertar()
+        //MODIFICAR
+        public static bool Modificar(Cuidades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(ciudades).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        //GUARDAR
+        public static bool Guardar(Cuidades ciudades)
+        {
+            if (!Existe(ciudades.CuidadId))
+                return Insertar(ciudades);
+            else
+                return Modificar(ciudades);
+        }
+
+        //ELIMINAR
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var ciudades = contexto.Cuidades.Find(id);
+                if (ciudades != null)
+                {
+                    contexto.Cuidades.Remove(ciudades);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        //BUSCAR
+        public static Cuidades Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Cuidades ciudades;
+            try
+            {
+                ciudades = contexto.Cuidades.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ciudades;
+        }
+      public static List<Cuidades> GetList(Expression<Func<Cuidades,bool>> criterio){
+          List<Cuidades>lista=new List<Cuidades>();
+          Contexto contexto = new Contexto();
+          try
+          {
+              //Obtener la lista y filtrarla segun el criterio recibido por parametro.
+               lista =contexto.Cuidades.Where(criterio).ToList();
+
+          }
+          catch(Exception)
+   {
+       throw;
+   }
+    finally
+    {
+        contexto.Dispose();
 
     }
+ return lista;
+}
+
+}
 }
--- Entidades/Cuidades.cs
+++ Entidades/Cuidades.cs
@@ -3,9 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Escarolin_P1_AP1.Entidades{
     public class Cuidades{
-        public int CuidadesId { get; set; }
+        [Key]
+        public int CuidadId { get; set; }
 
         public string Nombres { get; set; }
-        
+
     }
 }
